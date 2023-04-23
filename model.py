7179a4836b4a15bb71e7cb50d58df30d6fc2e04e
wamuelo <62719301+wamuelo@users.noreply.github.com>
2023-04-23 16:59:15 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -2,25 +2,21 @@ import torch
 import torch.nn as nn
 
 class MyModel(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size, num_layers=2, proj_size=0):
-        super().__init__()
+    def __init__(self, input_size, hidden_size, num_layers):
+        super(MyModel, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-
-        self.embed = nn.Embedding(input_size, hidden_size)
-        self.rnn = nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True, bidirectional=True, proj_size=proj_size)
-        self.fc = nn.Linear(hidden_size*2, output_size)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, input_size)
 
     def forward(self, x, h=None):
         if h is None:
-            h = self.init_hidden(x.shape[0])
-
-        x = self.embed(x)
-        out, h = self.rnn(x, h)
+            h = self.init_hidden(x.size(0))
+        out, h = self.lstm(x, h)
+        out = out.contiguous().view(-1, self.hidden_size)
         out = self.fc(out)
-
         return out, h
 
     def init_hidden(self, batch_size):
-        return (torch.zeros(self.num_layers*2, batch_size, self.hidden_size),
-                torch.zeros(self.num_layers*2, batch_size, self.hidden_size))
+        return (torch.zeros(self.num_layers, batch_size, self.hidden_size),
+                torch.zeros(self.num_layers, batch_size, self.hidden_size))
