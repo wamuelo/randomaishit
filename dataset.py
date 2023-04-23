f16756a88491f03ebccab059d1bbae169f42ae63
wamuelo <62719301+wamuelo@users.noreply.github.com>
2023-04-23 15:58:56 +0200

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -2,21 +2,25 @@ import torch
 import os
 
 class MyDataset(torch.utils.data.Dataset):
+    CHARACTERS = []
+
     def __init__(self, data_path, seq_length):
         self.seq_length = seq_length
-        self.chars = []
         self.data = []
 
         # Read data from all files in directory
         for filename in os.listdir(data_path):
             with open(os.path.join(data_path, filename), 'r') as f:
                 text = f.read()
-            self.chars += list(set(text))
             self.data += [text[i:i+self.seq_length] for i in range(0, len(text)-self.seq_length, self.seq_length)]
 
         # Map characters to indices
-        self.char_to_idx = { ch:i for i,ch in enumerate(sorted(self.chars)) }
-        self.idx_to_char = { i:ch for i,ch in enumerate(sorted(self.chars)) }
+        self.chars = sorted(list(set(''.join(self.data))))
+        self.char_to_idx = { ch:i for i,ch in enumerate(self.chars) }
+        self.idx_to_char = { i:ch for i,ch in enumerate(self.chars) }
+        
+        # Set the class attribute CHARACTERS
+        MyDataset.CHARACTERS = self.chars
 
     def __getitem__(self, index):
         x = [self.char_to_idx[ch] for ch in self.data[index]]
