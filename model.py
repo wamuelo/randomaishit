cf171561b3d302431f6372daa856c608d6324634
wamuelo <62719301+wamuelo@users.noreply.github.com>
2023-04-23 16:34:42 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -2,14 +2,13 @@ import torch
 import torch.nn as nn
 
 class MyModel(nn.Module):
-    def __init__(self, vocab_size, hidden_size):
-        super(MyModel, self).__init__()
-        self.embed = nn.Embedding(vocab_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=True)
-        self.linear = nn.Linear(hidden_size, vocab_size)
-
-    def forward(self, inputs, hidden=None):
-        embedded = self.embed(inputs)
-        outputs, hidden = self.lstm(embedded, hidden)
-        outputs = self.linear(outputs.view(-1, self.linear.in_features))
-        return outputs, hidden
+    def __init__(self, input_size, output_size, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.rnn = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=2, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, output_size)
+        
+    def forward(self, x, h):
+        out, h = self.rnn(x, h)
+        out = self.fc(out)
+        return out, h
